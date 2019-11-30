50ed7e6aa1c9b65d3ee8549796f7e24152f3417a
gasstondenis <53875088+gasstondenis@users.noreply.github.com>
2019-11-30 17:11:30 -0300

    Update ListaCuentas.java

--- src/cuentas/ListaCuentas.java
+++ src/cuentas/ListaCuentas.java
@@ -1,10 +1,9 @@
 package cuentas;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class ListaCuentas implements Serializable {
+public class ListaCuentas {
     public ArrayList<Cuenta> arrayCuentas = null;
 
 
@@ -18,38 +17,6 @@ public class ListaCuentas implements Serializable {
 
     public ArrayList<Cuenta> getArrayCuentas() {
         return arrayCuentas;
-    }
-
-    public Iterator<Cuenta> iterador() {
-        return new IteradorCuentas();
-    }
-
-    class IteradorCuentas implements Iterator<Cuenta>{
-        public int posicion = 0;
-        public boolean existe = false;
-
-
-        @Override
-        public boolean hasNext(){
-            return posicion<arrayCuentas.size();
-        }
-
-        @Override
-        public Cuenta next() {
-            Cuenta resultado = arrayCuentas.get(posicion);
-            posicion++;
-            existe = true;
-            return resultado;
-        }
-
-        @Override
-        public void remove() {
-            if (existe) {
-                arrayCuentas.remove(posicion - 1);
-                posicion--;
-                existe = false;
-            }
-        }
     }
 }
 
