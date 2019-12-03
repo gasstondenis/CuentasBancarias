ab65a3d6197027b590353da4f5e1dbe0ee0ea222
gasstondenis <53875088+gasstondenis@users.noreply.github.com>
2019-12-02 22:51:24 -0300

    Update AltaBaja.java

--- src/cuentas/AltaBaja.java
+++ src/cuentas/AltaBaja.java
@@ -4,20 +4,15 @@ package cuentas;
 import java.util.ArrayList;
 
 public class AltaBaja {
-    private int contadorCuentas;
     private boolean creado;
     private Almacenamiento almacenamiento = new Almacenamiento();
 
-    boolean crearCuenta(ArrayList arrayCuentas){
+    boolean crearCuenta(ArrayList<Cuenta> arrayCuentas){
         creado = almacenamiento.modArchivoCuenta(arrayCuentas);
         return creado;
     }
     boolean borrarCuenta(ArrayList<Cuenta> cuenta){
         creado = almacenamiento.modArchivoCuenta(cuenta);
         return creado;
-    }
-
-    int getContadorCuentas() {
-        return contadorCuentas;
     }
 }
