f88599ccc5c56053801c4420cabb93ea8540fcf9
gasstondenis <53875088+gasstondenis@users.noreply.github.com>
2019-12-03 01:21:07 -0300

    Update ListaCuentas.java

--- src/cuentas/ListaCuentas.java
+++ src/cuentas/ListaCuentas.java
@@ -3,18 +3,10 @@ package cuentas;
 import java.util.ArrayList;
 
 public class ListaCuentas {
-    public ArrayList<Cuenta> arrayCuentas = null;
+    public ArrayList<Cuenta> arrayCuentas;
     int i;
 
     public  ListaCuentas(){
         arrayCuentas = new ArrayList<Cuenta>();
-    }
-
-    public void setArrayCuentas(ArrayList<Cuenta> arrayCuentas) {
-        this.arrayCuentas = arrayCuentas;
-    }
-
-    public ArrayList<Cuenta> getArrayCuentas() {
-        return arrayCuentas;
     }
 }
